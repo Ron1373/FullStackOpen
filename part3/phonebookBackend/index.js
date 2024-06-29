4fb0b1091adc89407379516e2366c2226e48c350
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-06-28 23:52:11 -0700

    Complete Exercies 3.1-3.6

---
+++ part3/phonebookBackend/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send(`<h1>loading</h1>`);
+});
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end("not found");
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((note) => note.id !== id);
+  response.status(204).end;
+});
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  );
+});
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  if (!person.name) {
+    return response.status(400).json({ error: "person name is missing" });
+  }
+  if (!person.number) {
+    return response.status(400).json({ error: "person number is missing" });
+  }
+  if (persons.find((p) => p.name === person.name)) {
+    return response.status(400).json({ error: "name must be unique" });
+  }
+  const id = Math.floor(Math.random() * 1000) + 1;
+  person.id = id;
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+const PORT = 3001;
+app.listen(PORT);
