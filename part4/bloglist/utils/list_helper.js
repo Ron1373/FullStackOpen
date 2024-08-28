ff0e79e50d5af1f2d54392601c1544ecb5448b1e
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-08-28 23:23:07 +0545

    Add function to find the blog with the most likes and the corresponding test

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,7 +4,16 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  const { title, author, likes } = blogs.sort((a, b) => b.likes - a.likes)[0];
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
