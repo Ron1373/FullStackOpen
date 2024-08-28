de2d9de7ac86200c6792cb6fb87f7fd09fdbab24
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-08-28 22:07:23 +0545

    Refactored application into separate modules

---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,25 @@
+const logger = require("./logger");
+
+const requestLogger = (req, res, next) => {
+  logger.info("Method:", req.method);
+  logger.info("Path", req.path);
+  logger.info("Body:", req.body);
+  logger.info("---");
+
+  next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).json({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message);
+  if (error.name === "CastError") {
+    res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler };
