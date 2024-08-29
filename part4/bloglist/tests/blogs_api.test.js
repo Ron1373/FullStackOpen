681b8768b91edd289eba418e7a6baa73ef073b0e
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-08-29 14:24:05 +0545

    Add test for HTTP GET request to "/api/blogs"

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,32 @@
+const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blogs");
+const helper = require("../utils/helper");
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test.only("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test.only("there are five blogs", async () => {
+  const response = await api.get("/api/blogs");
+  assert.strictEqual(response.body.length, 6);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
