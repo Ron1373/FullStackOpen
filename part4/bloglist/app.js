4f1c24a2afdb4c806eadd4b748c7bd186825885c
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-08-30 12:14:51 +0545

    Refactored token extractor into middleware function

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -13,6 +13,7 @@ const middleware = require("./utils/middleware");
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 mongoose
   .connect(config.MONGODB_URI)
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -29,4 +29,19 @@ const errorHandler = (error, req, res, next) => {
   }
 };
 
-module.exports = { requestLogger, unknownEndpoint, errorHandler };
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer")) {
+    request.token = authorization.replace("Bearer ", "");
+  } else {
+    request.token = null;
+  }
+  next();
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+};
