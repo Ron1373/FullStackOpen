6fad0723aa090c3dc9ea9ae21be18bac2f96bc0b
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-08-31 14:00:24 +0545

    implement sorting by number of likes

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -3,9 +3,11 @@ import Blog from "./Blog";
 const BlogList = ({ blogs, setBlogs }) => (
   <>
     <h2>blogs</h2>
-    {blogs.map((blog) => (
-      <Blog key={blog.id} blog={blog} />
-    ))}
+    {blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
   </>
 );
 
