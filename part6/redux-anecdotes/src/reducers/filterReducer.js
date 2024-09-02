674dc335dd57dddb0b6b437dbb9d3bca464e7504
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-09-02 23:14:52 +0545

    Change the definition of the filter reducer and action creators to use the Redux Toolkit's createSlice function

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,14 +1,14 @@
-const reducer = (state = "", action) => {
-  switch (action.type) {
-    case "SET_FILTER":
-      return action.payload;
-    default:
-      return state;
-  }
-};
-export default reducer;
+import { createSlice } from "@reduxjs/toolkit";
 
-export const filterChange = (filter) => ({
-  type: "SET_FILTER",
-  payload: filter,
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    filterChange(state, action) {
+      return action.payload;
+    },
+  },
 });
+
+export default filterSlice.reducer;
+export const { filterChange } = filterSlice.actions;
