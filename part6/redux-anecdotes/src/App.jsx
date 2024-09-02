317e4e68de55dd6570b307cc7b42b25f12f53c8f
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-09-02 21:54:58 +0545

    Implement filtering for the anecdotes displayed to the user

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,12 @@
 import AnecdoteForm from "./components/Anecdoteform";
 import AnecdoteList from "./components/AnecdoteList";
+import Filter from "./components/Filter";
 
 const App = () => {
   return (
     <>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </>
--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { increaseVote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    switch (filter) {
+      case "":
+        return anecdotes;
+      default:
+        return anecdotes.filter((anecdote) =>
+          anecdote.content.includes(filter)
+        );
+    }
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
