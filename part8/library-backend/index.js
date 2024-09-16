3a11ee0973284b6945e0d2c90c5eec2152dda29a
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-09-16 22:28:08 +0545

    Implement mutation editAuthor to change birth year

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -114,6 +114,11 @@ const typeDefs = `
     published:Int!
     genres:[String!]!
     ):Book
+
+    editAuthor(
+      name:String!
+      setBornTo:Int!
+      ):Author
   }
 `;
 
@@ -149,11 +154,25 @@ const resolvers = {
     addBook: (root, args) => {
       const newBook = { ...args, id: uuid };
       books = books.concat(newBook);
-      const authorExists = authors.find((author) => author === newBook.author);
+      const authorExists = authors.find(
+        (author) => author.name === newBook.author
+      );
       if (!authorExists) {
         authors = authors.concat({ name: newBook.author, born: null });
       }
       return newBook;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === updatedAuthor.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
