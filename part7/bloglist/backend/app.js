9a326d122213a26ca7e60d1ada0a8dec40b4b70a
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-09-16 13:55:12 +0545

    FIx route handling during refresh

--- part7/bloglist/backend/app.js
+++ part7/bloglist/backend/app.js
@@ -9,7 +9,7 @@ const blogsRouter = require("./controllers/blogsRouter");
 const usersRouter = require("./controllers/usersRouter");
 const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
-
+const path = require("path");
 if (process.env.NODE_ENV === "test") {
   const testingRouter = require("./controllers/testing");
   app.use("/api/testing", testingRouter);
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "test") {
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -32,6 +32,10 @@ mongoose
 app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
