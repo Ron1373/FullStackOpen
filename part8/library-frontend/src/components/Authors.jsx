4b3e6b89be890c6d42baf5419df751f653c4765a
Ronaldo Shrestha <149232447+Ron1373@users.noreply.github.com>
2024-09-20 15:27:52 +0545

    Implement option to add new books

--- part8/library-frontend/src/components/Authors.jsx
+++ part8/library-frontend/src/components/Authors.jsx
@@ -1,14 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "./queries";
 
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors {
-      name
-      born
-      bookCount
-    }
-  }
-`;
 const Authors = () => {
   const result = useQuery(ALL_AUTHORS);
 
--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -1,4 +1,6 @@
+import { useMutation } from "@apollo/client";
 import { useState } from "react";
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from "./queries";
 
 const NewBook = () => {
   const [title, setTitle] = useState("");
@@ -7,11 +9,15 @@ const NewBook = () => {
   const [genre, setGenre] = useState("");
   const [genres, setGenres] = useState([]);
 
+  const [createPerson] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+  });
   const submit = async (event) => {
     event.preventDefault();
 
-    console.log("add book...");
-
+    createPerson({
+      variables: { title, author, published: Number(published), genres },
+    });
     setTitle("");
     setPublished("");
     setAuthor("");
